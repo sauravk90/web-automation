7d9263c9849ffac20753da97c6df6290e395b063
Saurav.Kumar <saurav.kumar90@hotmail.com>
2019-04-06 13:30:43 +0530

    Moved Constants.py

--- Constants.py
+++
@@ -1,9 +0,0 @@
-import os
-'''
-Class to store all constants to be used project wide
-'''
-URL = 'https://www.socialchains.io/home-en'
-DOWNLOAD_PATH = os.getcwd() + '\\reports\\'
-TEST_DATA_PATH = os.getcwd() + '\\testdata\\'
-SIGN_IN='SIGN IN'
-DEFAULT_WAIT=10
